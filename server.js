6b3824c0a9585f0a3ef8b78e58d13b728fc8fea5
vinaytallapally <vinaytallapally660@gmail.com>
2016-09-30 21:17:45 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,6 +5,59 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
+
+var articleone={
+    title:'Article one | T Venkatesh',
+    heading:'Article one',
+    date:'sept 30 , 2016',
+    content: `  <p>
+                My name is T venkatesh   My name is T venkatesh
+                 My name is T venkatesh
+                  My name is T venkatesh
+                 My name is T venkatesh
+            </p>
+            <p>
+                 My name is T venkatesh   My name is T venkatesh   My name is T venkatesh   My name is T venkatesh
+                 My name is T venkatesh     My name is T venkatesh    My name is T venkatesh   My name is T venkatesh
+            </p>
+               <p>
+                My name is T venkatesh   My name is T venkatesh
+                 My name is T venkatesh
+                  My name is T venkatesh
+                 My name is T venkatesh
+            </p> `
+};
+
+var htmltemplate=`
+<html>
+    <head>
+        <title>
+             ${title}
+        </title>
+        <meta name="viewport" content="width-device-width,initial-scale=1"/>
+           <link href="/ui/style.css" rel="stylesheet" />
+           
+    </head>
+    <body>
+        <div class="container">
+        <div>
+            <a href="/">Home</a>
+        </div>
+        <hr/>
+        <h3>
+            ${heading}
+        </h3>
+        <div>
+          ${date}
+        </div>
+        
+        <div>
+         ${content}
+    </div>
+</div>
+    </body>
+</html>
+`;
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
