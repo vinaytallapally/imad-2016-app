9d9e3657aa5e98e61fff218dd42a50bc582702f2
vinaytallapally <vinaytallapally660@gmail.com>
2016-10-04 18:05:24 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -68,13 +68,14 @@ var htmlTemplate=`
              ${title}
         </title>
         <meta name="viewport" content="width-device-width,initial-scale=1"/>
-           <link href="/ui/style.css" rel="stylesheet" />
-           <style>
+            <style>
          img :
     margin: 5px;
       text-align: center;
     width: 100px;
            </style>
+           <link href="/ui/style.css" rel="stylesheet" />
+       
     </head>
     <body>
         <div class="container">
