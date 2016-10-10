af076df484106a703fd929146b9f5c768398d9ea
vinaytallapally <vinaytallapally660@gmail.com>
2016-10-10 14:30:56 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -92,6 +92,15 @@ var htmlTemplate=`
 `;
 return htmlTemplate;
 }
+
+var names=[]; // java script object
+app.get('/submit-name/:name',function(req,res){
+var name=req.params.name;
+names.push(name); 
+// JSON : Javascript Object Notation
+rs.send(JSON.stringify(names));
+});
+
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
@@ -121,13 +130,6 @@ app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
 
-var names=[]; // java script object
-app.get('/submit-name/:name',function(req,res){
-var name=req.params.name;
-names.push(name); 
-// JSON : Javascript Object Notation
-rs.send(JSON.stringify(names));
-});
 
 var port = 8080; // Use 8080 for local development because you might already have apache running on 80
 app.listen(8080, function () {
