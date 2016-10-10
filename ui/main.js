531dbc8d4b23c84aa670ff3d4c1fa46dc0bc64d6
vinaytallapally <vinaytallapally660@gmail.com>
2016-10-10 14:07:36 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -25,3 +25,19 @@ button.onclick = function()
     request.open('GET','http://vinaytallapally.imad.hasura-app.io/counter',true);
     request.send(null);
 };
+var nameInput=document.getElementById('name');
+var name=nameInput.value;
+var submit=document.getElementById('submit_button');
+submit.onclick=function(){
+    //making request to the server and sending name
+    
+    //capture a list of names and render the list
+    var names=['name1','name2','name3','name4'];
+    var list='';
+    for(var i=0;i<names.length;i++)
+    {
+        list+='<li>'+names[i]+'</li>';
+    }
+   var ul=document.getElementById("namelist") ;
+   ul.innerHTML=list;
+};
