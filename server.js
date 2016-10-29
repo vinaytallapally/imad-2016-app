7119c0f624c8e93d00b02e9f2f21709ce449398c
vinaytallapally <vinaytallapally660@gmail.com>
2016-10-29 18:41:09 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,7 +1,14 @@
 var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
-
+var Pool=require('pg').pool;
+var config={
+    user:'vinaytallapally',
+    database:'vinaytallapally',
+    host:'db.imad.hasura-app.io',
+    port:'5432',
+    password:process.env.DB_PASSWORD
+};
 var app = express();
 app.use(morgan('combined'));
 
