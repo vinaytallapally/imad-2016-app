208a19bab5c59dd05508391010f63ac8c2da77e7
vinaytallapally <vinaytallapally660@gmail.com>
2016-10-29 19:07:30 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -108,6 +108,13 @@ names.push(name);
 res.send(JSON.stringify(names));
 });  */
 
+ var names=[]; // java script object
+app.get('/submit-name',function(req,res){ // submit-name?name=xxxx
+var name=req.query.name;
+names.push(name); 
+// JSON : Javascript Object Notation
+res.send(JSON.stringify(names));
+}); 
 
 
 app.get('/', function (req, res) {
