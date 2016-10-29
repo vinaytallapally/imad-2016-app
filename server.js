72248468a57f2ace8e96785231c705e1ed02db76
vinaytallapally <vinaytallapally660@gmail.com>
2016-10-29 18:45:27 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -120,10 +120,21 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
+var pool=new Pool(config);
 app.get('/test-db',function(req,res)
 {
  // make a select request
  // return a response with results
+ pool.query('SELECT * FROM test',function(err,result){
+     if(err)
+     {
+         res.status(500).send(err.toString());
+     }
+     else
+     {
+         res.send(JSON.stringify(result.rows));
+     }
+ });
 });
 var counter=0;
 app.get('/counter', function (req, res) {
